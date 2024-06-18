db1909e4fc427ccbbdd7fd1f1663a87ae4344eae
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-06-18 02:40:48 -0400

    We finidhed implementing 03-queue

--- implementation/03-queue.js
+++ implementation/03-queue.js
@@ -5,17 +5,46 @@ class Queue {
     constructor() {
         this.head = null;
         this.tail = null;
+        this.length = 0;
     }
 
     enqueue(val) {
         // Add node to end of queue (linked list)
+        // We are pushing the val in the nodes
 
+        const node = new SinglyLinkedNode(val);
+
+        if (!this.tail) {
+            this.tail = node;
+            this.head = this.tail;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+
+        this.length++;
+
+        return this.length;
         // Write your hypothesis on the time complexity of this method here
+        // O(1);
     }
 
     dequeue() {
         // Remove node from front of queue (linked list)
-        
+
+        if (!this.head) return null;
+
+        let node = this.head;
+
+        if (!this.head.next) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+        }
+
+        this.length--;
+        return node.value;
         // Write your hypothesis on the time complexity of this method here
     }
 
